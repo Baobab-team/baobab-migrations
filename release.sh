a20b7dfb1eb9072725b410b68b5b6a289eeb629d
Sylvain Dégué <sylvain.degue@gmail.com>
2020-07-30 21:43:05 -0400

    update release.sh

--- release.sh
+++ release.sh
@@ -1,8 +1,10 @@
 #generate file
 rm liquibase.properties
 cat <<EOT >> liquibase.properties
-changeLogFile=db.changelog-master.yml
-url=jdbc:$DATABASE_URL
+changeLogFile:db.changelog-master.yml
+url: jdbc:$DATABASE_URL
+driver: org.postgresql.Driver
+classpath:  liquibase/lib/postgresql-42.2.14.jar
 EOT
 
 #run migrations
